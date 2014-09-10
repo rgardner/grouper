c4f8ab624721684fb907d25279d635f32c81b2ea
Robert Gardner <rhg259@nyu.edu>
2014-09-10 15:48:43 -0400

    Add person creation and skill model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  root 'static_pages#home'
-  match '/signin', to: 'users#new', via: 'get'
+  resources :people, only: [:new, :create]
+
+  root 'people#new'
+  match '/groups', to: 'static_pages#home', via: 'get'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140910184133) do
+ActiveRecord::Schema.define(version: 20140910193336) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,12 @@ ActiveRecord::Schema.define(version: 20140910184133) do
     t.string   "name"
     t.string   "school"
     t.text     "skills",     default: [], array: true
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "skills", force: true do |t|
+    t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
